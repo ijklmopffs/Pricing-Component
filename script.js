e841aec133babd80f6eacef4b3e2c64595e9e110
Nuel Elekwachi <elekwaeke300@gmail.com>
2023-05-26 01:24:23 +0100

    Done

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
 const toggle = document.getElementById("toggle");
-const amounts = document.getElementsByClassName("amount");
 
-toggle.addEventListener("change", function () {
+toggle.addEventListener("click", function () {
   if (this.checked) {
-    for (let i = 0; i < amounts.length; i++) {
-      amounts[i].textContent = "$199.99";
-    }
+    one.classList.remove("annually");
+    two.classList.add("annually");
+    three.classList.remove("annually");
+    four.classList.add("annually");
+    five.classList.remove("annually");
+    six.classList.add("annually");
+    console.log("worked");
   } else {
-    for (let i = 0; i < amounts.length; i++) {
-      amounts[i].textContent = "$24.99";
-    }
+    one.classList.add("annually");
+    two.classList.remove("annually");
+    three.classList.add("annually");
+    four.classList.remove("annually");
+    five.classList.add("annually");
+    six.classList.remove("annually");
   }
 });
